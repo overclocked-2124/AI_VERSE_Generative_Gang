65b39491d69148c54ad9f7fb7689af7c2a558f11
Sumukha_Upadhyaya <sumukhaupadhyaya@gmail.com>
2025-03-19 17:08:21 +0530

    added speechtotext logic

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def index():
     return render_template('index.html')
 
-@app.route('/about/')
+@app.route('/about')
 def about():
     return render_template('about.html')
 

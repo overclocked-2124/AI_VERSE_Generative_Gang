8ef712cb8b5394a0c88c528c139357fe1f3ea295
Sumukha_Upadhyaya <sumukhaupadhyaya@gmail.com>
2025-03-22 13:37:15 +0530

    added missing urlfor in import

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request,url_for
 from utils.speechtotext import WhisperVAD
 import ollama
 
